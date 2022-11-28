e33dfa8458c938b76a57856458acfdd59bc78545
Jean-Elie <jeaneliekubembula@gmail.com>
2022-11-29 00:28:31 +0100

    Creation des models Libre et Category

--- api/library/models.py
+++ api/library/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    label = models.CharField(max_length=20, verbose_name="label")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.label)
+
+
+class Livre(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="livre",
+                                 verbose_name="Project's category")
+    nom = models.CharField(max_length=50)
+    auteur = models.CharField(max_length=50)
+    nombreDePage = models.IntegerField()
+    description = models.TextField(blank=True)
+    prix = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return str(self.nom)
